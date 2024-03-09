5839999fe6c559cd0aff909c87c19755759fa856
ggemelle <gabriel.spgemelli@gmail.com>
2024-03-09 10:46:07 -0300

    entrega pratica-0

---
+++ pratica-0/src/index.js
@@ -0,0 +1,9 @@
+// Exemplo de uso prático
+let nome = "Gabriel GEC1636";
+function saudacaoPersonalizada(nome) {
+    console.log("Olá, " + nome + "!");
+}
+saudacaoPersonalizada(nome);
+
+let modulo = require('./module.js');
+modulo.saudacao();
---
+++ pratica-0/src/module.js
@@ -0,0 +1,7 @@
+// arquivo modulo.js
+module.exports = {
+    mensagem: 'Testando modulo: Gabriel GEC1636!',
+    saudacao: function() {
+      console.log(this.mensagem);
+    }
+  };
